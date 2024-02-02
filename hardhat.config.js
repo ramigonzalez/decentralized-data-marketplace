e77dcfd54f4a231f6e2cfd1fb9ac648ef16b41d9
Ramiro Gonzalez <ramirogc21@gmail.com>
2024-02-02 18:35:14 -0300

    hardhat config

---
+++ hardhat.config.js
@@ -0,0 +1,27 @@
+require("@nomicfoundation/hardhat-toolbox");
+require("@nomiclabs/hardhat-ethers");
+require('dotenv').config();
+
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  defaultNetwork: "localhost",
+  networks: {
+    hardhat: {},
+    sepolia: {
+      url: process.env.SEPOLIA_API_URL,
+      accounts: [process.env.PRIVATE_KEY],
+    },
+    localhost: {
+      url: "http://127.0.0.1:8545",
+    }
+  },
+  solidity: {
+    version: "0.8.20",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 1000,
+      },
+    },
+  },
+};

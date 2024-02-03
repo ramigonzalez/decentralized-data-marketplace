93a841517121c3545133b0642b40327f3a89b170
Ramiro Gonzalez <ramirogc21@gmail.com>
2024-02-02 21:28:13 -0300

    natspec comments

--- contracts/DecentralizedDataMarket.sol
+++ contracts/DecentralizedDataMarket.sol
@@ -5,11 +5,13 @@ pragma solidity ^0.8.20;
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/access/AccessControl.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
-import "@openzeppelin/contracts/utils/Strings.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 
+/// @notice This contact intends to provide a Data Decentralization Marketplace implementation
+/// @dev Comment follow the Ethereum ´Natural Specification´ language format (´natspec´)
 contract DecentralizedDataMarket is ERC721URIStorage, AccessControl, Ownable {
 
+    /// @dev Access control constants
     bytes32 public constant DATA_PROVIDER_ROLE = keccak256("DATA_PROVIDER_ROLE");
     bytes32 public constant CONSUMER_ROLE = keccak256("CONSUMER_ROLE");
     bytes32 public constant ADMIN_ROLE = keccak256("ADMIN_ROLE");
@@ -26,10 +28,12 @@ contract DecentralizedDataMarket is ERC721URIStorage, AccessControl, Ownable {
         DataCategory category;
     }
 
+    /// @dev STATE MAPPINGS
     mapping(uint256 => uint256) public tokenPrices;
     mapping(uint256 => DataCategory) public dataCategories;
     mapping(address => mapping(uint256 => Subscription)) public subscriptions;
 
+    /// EVENTS
     event DataTokenCreated(uint256 indexed tokenId, string indexed dataHash, DataCategory indexed category, address creator);
     event DataListedForSale(uint256 indexed tokenId, uint256 indexed price, address creator);
     event SubscriptionCreated(address creator, uint256 indexed tokenId, uint256 duration);
@@ -39,16 +43,25 @@ contract DecentralizedDataMarket is ERC721URIStorage, AccessControl, Ownable {
         _; // Continue with the rest of the function execution
     }
 
+    /// @notice Initialize the state of the contract
+    /// @dev Admin role is granted to deployer
+    /// @dev The deployer is the owner of the protocol
     constructor() ERC721("DecentralizedDataMarket", "DATAM") Ownable(msg.sender) {
         _grantRole(ADMIN_ROLE, msg.sender);
     }
 
+    /// @notice Mints a new data token representing a data asset on IPFS. Also transfer minting fees to the protocol owner
+    /// @dev Throw if msg.sender has not DATA_PROVIDER_ROLE role.
+    /// @dev Throw if msg.value < mintFee. Message: "Insufficient fee"
+    /// @dev Throw if ipfsHash length is zero. Message: "Invalid IPFS hash"
+    /// @param ipfsHash The hash obtained after store the data asset into IPFS by a client
+    /// @param category An access level determined by the data asset owner
     function mintDataToken(string memory ipfsHash, DataCategory category) public payable onlyRole(DATA_PROVIDER_ROLE) {
         // Check if sender has sufficient balance
         require(msg.value >= mintFee, "Insufficient fee");
 
         // Check if sender has sufficient balance
-        require(bytes(ipfsHash).length > 0, "Failed to store data on IPFS");
+        require(bytes(ipfsHash).length > 0, "Invalid IPFS hash");
 
         // Mint the token and link it to the IPFS hash string
         uint256 tokenId = _tokenIdCounter + 1;
@@ -65,14 +78,23 @@ contract DecentralizedDataMarket is ERC721URIStorage, AccessControl, Ownable {
         emit DataTokenCreated(tokenId, ipfsHash, category, msg.sender);
     }
 
-    // Marketplace & Revenue Sharing
+    /// @notice Lists a data token for sale at a specified price
+    /// @dev Throw if msg.sender has not DATA_PROVIDER_ROLE role.
+    /// @dev Throw if msg.sender is not the token owner. Message: "Only token owner can perform this action"
+    /// @dev Throw if price is zero. Message: "Price must be greater than zero"
+    /// @param tokenId The token id of the desired data asset 
+    /// @param price The price to list the data asset
     function listDataForSale(uint256 tokenId, uint256 price) public onlyRole(DATA_PROVIDER_ROLE) onlyOwnerOf(tokenId) {
         require(price > 0, "Price must be greater than zero");
         tokenPrices[tokenId] = price;
         emit DataListedForSale(tokenId, price, msg.sender);
     }
 
-    // Purchase a data asset
+    /// @notice Enables a consumer to purchase a data token. Also transfer platform fees to the protocol owner
+    /// @dev Throw if msg.sender has not CONSUMER_ROLE role.
+    /// @dev Throw if tokenPrice is zero. Message: "Token has not price set yet"
+    /// @dev Throw if msg.value is less than tokenPrice. Message: "Insufficient funds"
+    /// @param tokenId The token id of the desired data asset
     function purchaseData(uint256 tokenId) public payable onlyRole(CONSUMER_ROLE) {
         uint256 tokenPrice = priceOf(tokenId);
         require(tokenPrice > 0, "Token has not price set yet");
@@ -84,17 +106,29 @@ contract DecentralizedDataMarket is ERC721URIStorage, AccessControl, Ownable {
         payable(owner()).transfer(platformValue); 
     }
 
+    /// @notice Retrieve the price of a listed token
+    /// @param tokenId The token id of the desired data asset
     function priceOf(uint256 tokenId) public view returns (uint256) {
         return tokenPrices[tokenId];
     }
 
+    /// @notice Grants a consumer access to data based on their subscription retrieving the token uri.
+    /// @dev Throw if msg.sender has not CONSUMER_ROLE role.
+    /// @dev Throw if the subscription expired. Message: "Subscription expired"
+    /// @dev Throw if the subscription does not match the category of access level. Message: "Insufficient access level"
+    /// @param tokenId The token id of the desired data asset
     function requestDataAccess(uint256 tokenId) public payable onlyRole(CONSUMER_ROLE) returns (string memory) {
         Subscription memory subscription = subscriptions[msg.sender][tokenId];
         require (subscription.expiresAt <= block.timestamp, "Subscription expired");
-        require (subscription.category <= dataCategories[tokenId], "insufficient access level");
+        require (subscription.category <= dataCategories[tokenId], "Insufficient access level");
         return tokenURI(tokenId);
     }
 
+    /// @notice Allows a consumer to subscribe to a data token for a specified duration.
+    /// @dev Throw if msg.sender has not CONSUMER_ROLE role.
+    /// @dev Throw if the tokenId does not exist. Message: "Token does not exist"
+    /// @dev Throw if the msg.value is less than the monthlyFee. Message: "Insufficient funds"
+    /// @param tokenId The token id of the desired data asset
     function subscribeToToken(uint256 tokenId) public payable onlyRole(CONSUMER_ROLE) {
         require(tokenId <= _tokenIdCounter, "Token does not exist");
         require(msg.value >= monthlyFee, "Insufficient funds");
@@ -120,37 +154,60 @@ contract DecentralizedDataMarket is ERC721URIStorage, AccessControl, Ownable {
         emit SubscriptionCreated(msg.sender, tokenId, duration);
     }
 
-    // Access control
+    /// @notice Retrieves the IPFS hash associated with a data token
+    /// @dev Throw if msg.sender has not DATA_PROVIDER_ROLE role.
+    /// @dev Throw if msg.sender is not the token owner. Message: "Only token owner can perform this action"
+    /// @param tokenId The token id of the desired data asset
     function getDataURI(uint256 tokenId) public view onlyRole(DATA_PROVIDER_ROLE) onlyOwnerOf(tokenId) returns (string memory) {
         // Get the IPFS hash
         return tokenURI(tokenId);
     }
 
+    /// @notice Grants the DATA_PROVIDER_ROLE to an account.
+    /// @dev Throw if msg.sender has not ADMIN_ROLE role.
+    /// @param account The address to grant the role
     function grantDataProviderRole(address account) public onlyRole(ADMIN_ROLE) {
         _grantRole(DATA_PROVIDER_ROLE, account);
     }
 
+    /// @notice Grants the ADMIN_ROLE to an account.
+    /// @dev Throw if msg.sender has not ADMIN_ROLE role.
+    /// @param account The address to grant the role
     function grantAdminRole(address account) public onlyRole(ADMIN_ROLE) {
         _grantRole(ADMIN_ROLE, account);
     }
 
+    /// @notice Grants the CONSUMER_ROLE to an account.
+    /// @dev Throw if msg.sender has not ADMIN_ROLE role.
+    /// @param account The address to grant the role
     function grantConsumerRole(address account) public onlyRole(ADMIN_ROLE) {
         _grantRole(CONSUMER_ROLE, account);
     }
 
+    /// @notice Revokes the DATA_PROVIDER_ROLE from an account.
+    /// @dev Throw if msg.sender has not ADMIN_ROLE role.
+    /// @param account The address to grant the role
     function revokeDataProviderRole(address account) public onlyRole(ADMIN_ROLE) {
         _revokeRole(DATA_PROVIDER_ROLE, account);
     }
 
+    /// @notice Revokes the ADMIN_ROLE from an account (preventing self-revocation).
+    /// @dev Throw if msg.sender has not ADMIN_ROLE role.
+    /// @dev Throw if msg.sender is the same account sent by param. Message: "Cannot revoke own admin role"
+    /// @param account The address to grant the role
     function revokeAdminRole(address account) public onlyRole(ADMIN_ROLE) {
         require(account != msg.sender, "Cannot revoke own admin role"); // Prevent self-revocation
         _revokeRole(ADMIN_ROLE, account);
     }
 
+    /// @notice Revokes the CONSUMER_ROLE from an account.
+    /// @dev Throw if msg.sender has not ADMIN_ROLE role.
+    /// @param account The address to grant the role
     function revokeConsumerRole(address account) public onlyRole(ADMIN_ROLE) {
         _revokeRole(CONSUMER_ROLE, account);
     }
 
+    /// @notice Checks if the contract supports a given interface.
     function supportsInterface(bytes4 interfaceId) public view override(AccessControl, ERC721URIStorage) returns (bool) {
         if (interfaceId == type(IERC165).interfaceId) {
             return true; // Always support IERC165
